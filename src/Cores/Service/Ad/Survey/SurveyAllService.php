18aef6f82a2eca662f1be79f79a8b804d0364cb9
qwf <>
2025-05-13 22:01:19 +0800

    first commit

---
+++ src/Cores/Service/Ad/Survey/SurveyAllService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Cores\Service\Ad\Survey;
+
+use App\Cores\Service\Service;
+
+class SurveyAllService extends Service
+{
+    protected function path()
+    {
+        return '/ad/api/survey/all';
+    }
+}
