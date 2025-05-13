93a5a9f3133ad6f27ed2263ddbd67f6dced7dbd6
qwf <>
2025-05-13 14:44:45 +0800

    first commit

---
+++ src/Cores/Core.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Cores;
+
+use Exception;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+abstract class Core
+{
+    protected $data = [];
+
+    public function __construct()
+    {
+        $this->data = new Data();
+    }
+
+    public static function init()
+    {
+        return new static();
+    }
+
+    /**
+     * @return $this
+     * @throws Exception
+     */
+    public function run()
+    {
+        try {
+            $this->request();
+            $this->execute();
+            $this->response();
+        } catch (Exception $exception) {
+            Log::error(get_class($this) . '错误', [
+                'error' => $exception->getMessage(),
+                'data' => $this->data->all(),
+            ]);
+            throw $exception;
+        }
+        return $this;
+    }
+
+    abstract protected function execute();
+
+    public function setRequests($requests)
+    {
+        $this->data->setItems($requests);
+        return $this;
+    }
+
+    public function setRequest($key, $value)
+    {
+        $this->data->setItem($key, $value);
+        return $this;
+    }
+
+    public function getResponses()
+    {
+        return $this->data->all();
+    }
+
+    public function getResponse($key, $default = null)
+    {
+        return $this->data->getItem($key, $default);
+    }
+
+    /**
+     * @param $rules
+     * @param array $messages
+     * @throws Exception
+     */
+    public function validate($rules, $messages = [])
+    {
+        if (empty($rules)) {
+            return;
+        }
+        $rules = $this->snake($rules);
+        if (!empty($messages)) {
+            $messages = $this->snake($messages);
+            $messages = Arr::dot($messages);
+        }
+        $validate = Validator::make($this->data->all(), $rules, $messages);
+        if ($validate->fails()) {
+            throw new Exception($validate->errors()->first());
+        }
+    }
+
+    final protected function snake($array)
+    {
+        $results = [];
+        foreach ($array as $key => $value) {
+            $camelKey = Str::snake($key);
+            if (is_array($value) && !empty($value)) {
+                $results[$camelKey] = $this->snake($value);
+            } else {
+                $results[$camelKey] = $value;
+            }
+        }
+        return $results;
+    }
+
+    protected function request()
+    {
+
+    }
+
+    protected function response()
+    {
+
+    }
+}
---
+++ src/Cores/Feature/Feature.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Cores\Features;
+
+use App\Cores\Core;
+use Exception;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+abstract class Feature extends Core
+{
+    public function run()
+    {
+        DB::beginTransaction();;
+        try {
+            parent::run();
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            $this->exception($exception);
+        }
+        return $this;
+    }
+
+    protected function exception(Exception $exception)
+    {
+        $this->error($exception->getMessage());
+    }
+
+    protected function error($message = 'error', $data = [])
+    {
+        $this->setResponses([
+            'success' => false,
+            'errorMessage' => $message,
+            'data' => $data,
+            'errorCode' => 0,
+            'showType' => 2,
+        ]);
+        return $this;
+    }
+
+    protected function success($data = [], $message = '')
+    {
+        $this->setResponses([
+            'success' => true,
+            'errorMessage' => $message,
+            'data' => $data,
+            'errorCode' => 0,
+            'showType' => 0,
+        ]);
+        return $this;
+    }
+
+    protected function paginate(Builder $model, $callback = null)
+    {
+        $perPage = $this->getPerPage();
+        $page = $this->getPage();
+        $model = $model->paginate($perPage, ['*'], 'current', $page);
+        $items = Collection::make($model->items())->toArray();
+        if ($callback) {
+            $items = call_user_func($callback, $items);
+        }
+        $this->setResponses([
+            'data' => $items,
+            'total' => $model->total(),
+            'pageSize' => $model->perPage(),
+            'current' => $model->currentPage(),
+            "success" => true,
+            "message" => "success",
+        ]);
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getPage()
+    {
+        return $this->data->getItem('current');
+    }
+
+    /**
+     * @return int
+     */
+    protected function getPerPage()
+    {
+        return $this->data->getItem('page_size');
+    }
+
+    final protected function camel($array)
+    {
+        $results = [];
+        foreach ($array as $key => $value) {
+            $camelKey = Str::camel($key);
+            if (is_array($value) && !empty($value)) {
+                $results[$camelKey] = $this->camel($value);
+            } else {
+                $results[$camelKey] = $value;
+            }
+        }
+        return $results;
+    }
+
+    protected function setResponses($data = [])
+    {
+        $data = $this->camel($data);
+        $this->data->setItem('responses', $data);
+    }
+
+    public function getResponses()
+    {
+        return (array) $this->data->getItem('responses');
+    }
+}
