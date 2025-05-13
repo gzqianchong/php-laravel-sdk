20309d67bd56bfcfb401a00d97924b8fbb34b25e
qwf <>
2025-05-13 15:03:44 +0800

    init

---
+++ src/Cores/Service/Service.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Cores\Service;
+
+use App\Cores\Core;
+use Illuminate\Support\Facades\Http;
+
+abstract class Service extends Core
+{
+    abstract protected function path();
+
+    protected function host()
+    {
+        if (config('app.env') !== 'production') {
+            return 'http://' . config('app.env') . '-apigateway.qcyyds.com/';
+        }
+        return 'http://apigateway.qcyyds.com/';
+    }
+
+    public function setBody($body = [])
+    {
+        $this->data->setItem('body', $body);
+        return $this;
+    }
+
+    protected function execute()
+    {
+        $this->http();
+    }
+
+    protected function http()
+    {
+        $url = trim($this->host(), '/') . '/' . trim($this->path(), '/');
+        $body = $this->data->getItem('body');
+        $http = Http::post($url, $body);
+        $this->data->setItem('httpResponses', $http->json());
+    }
+
+    public function getHttpResponses()
+    {
+        return (array) $this->data->getItem('httpResponses');
+    }
+}
